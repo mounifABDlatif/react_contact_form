6a111a3ac6f328e25c41475a7ed8fdccfcdf120f
Mounif Abdoulatif <mounif2012@gmail.com>
2021-12-01 19:51:11 +0100

    màj form. control des input valide

--- src/App.js
+++ src/App.js
@@ -7,23 +7,77 @@ const App = () => {
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
 
+	const isEmail = () => { // sert à vérifier le format de l'email 
+		let mail = document.getElementById('not-mail'); // pointe sur le label pour afficher le message d'erreur
+		let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+		if (email.match(regex)) { //on vérifie si le mail match avec la reagex
+
+			mail.style.display = 'none';
+			return true;
+
+		} else {
+
+			mail.style.display = 'block';
+			mail.style.animation = 'dongle 1s';
+			setTimeout(() => { // permet de relancer l'animation à chaque fois qu'il y a une erreur dans le format du mail
+				mail.style.animation= 'none';
+			}, 1000);
+			return false;
+
+		}
+	};
+
+	const failMessage = (messageError) => {
+		let formMess = document.querySelector('.form-message');
+
+		formMess.innerHTML = messageError; //message d'erreur que nous allons mettre en paramètre en fonction du type d'erreur
+		formMess.style.opacity = '1';
+		formMess.style.background = 'rgb(253,87,87)';
+
+		document.getElementById('name').classList.add('error');
+		document.getElementById('email').classList.add('error');
+		document.getElementById('message').classList.add('error');
+	}
+
+	const successMessage = () => {
+		let formMess = document.querySelector('.form-message');
+		formMess.innerHTML = 'Message envoyé 👍👍 Nous vous recontacterons dès quee possible.';
+		formMess.style.background = '#00c1ec';
+		formMess.style.opacity = '1';
+
+		document.getElementById('name').classList.remove('error');
+		document.getElementById('email').classList.remove('error');
+		document.getElementById('message').classList.remove('error'); //on enlève les messages d'erreurs
+
+		setTimeout(() => { //enlève le successMessage au bout de qq secondes
+			formMess.style.opacity = '0';
+		}, 5000);
+	}
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		sendFeedback("template_utbcrmx", { // sendFeedback() récupre les variables que l'on retrouve dans le template
-			name,
-			company,
-			phone,
-			email,
-			message,
-		});
+		if (name && isEmail() && message) { //cette condition va rendre obligatoire le renseignement des élèments importants
+			sendFeedback("template_utbcrmx", {
+				// sendFeedback() récupre les variables que l'on retrouve dans le template
+				name,
+				company,
+				phone,
+				email,
+				message,
+			});
+		} else {
+			failMessage("Merci de remplir correctement les champs requis *");
+		}
 	};
 
-	const sendFeedback = (templateId, variables) => { // on lance une fonction async pour récupéré les données si cela fonction ou envoyer un message d'erreur si l'action n'est pa valide
+	const sendFeedback = (templateId, variables) => {
+		// on lance une fonction async pour récupéré les données si cela fonction ou envoyer un message d'erreur si l'action n'est pa valide
 		window.emailjs
 			.send("gmail", templateId, variables)
 			.then((res) => {
-				console.log("success !"); // tout les "set" suivant vont remettre le template à zéro
+				successMessage(); // tout les "set" suivant vont remettre le template à zéro
 				setName("");
 				setCompany("");
 				setPhone("");
@@ -31,9 +85,9 @@ const App = () => {
 				setMessage("");
 			})
 			.catch(
-				(err) =>
-					(document.querySelector(".form-message").innerHTML =
-						"Oops 😥! Une erreur s'est produite, veuillez réessayer.")
+				(err) => {
+					failMessage("Oops 😥! Une erreur s'est produite, veuillez réessayer.")
+				}
 			);
 	};
 
